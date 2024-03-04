a4fbdd6baa9745fc538308576cc0233d1c75550e
natro92 <2997453446@qq.com>
2024-03-04 12:11:33 +0800

    first

---
+++ alist-detector.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@Project ：alist-detector 
+@File    ：alist-detector.py
+@desc    ：
+@Author  ：@Natro92
+@Date    ：2024/3/4 11:55 
+@Blog    : natro92.github.io
+@Contact : 2997453446@qq.com
+"""
+
+if __name__ == '__main__':
+    print("hello")
---
+++ alist/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@Project ：alist-detector 
+@File    ：main.py
+@desc    ：
+@Author  ：@Natro92
+@Date    ：2024/3/4 11:56 
+@Blog    : natro92.github.io
+@Contact : 2997453446@qq.com
+"""
